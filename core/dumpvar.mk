47efdf01bf72c21a4b912199422877277e303817
Paul Keith <javelinanddart@bestas.gr>
2017-04-18 07:22:01 -0500

    dumpvar: Dump TARGET_USE_SDCLANG
    * But only do it if SDCLANG is detected locally
    
    Change-Id: I4e9556bfcefc0321351601471b6f5587ff5a42d4
    Signed-off-by: Paul Keith <javelinanddart@bestas.gr>

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -16,6 +16,11 @@ print_build_config_vars := \
   HOST_OS_EXTRA \
   OUT_DIR
 
+ifeq ($(SDCLANG),true)
+print_build_config_vars += \
+  TARGET_USE_SDCLANG
+endif
+
 ifneq ($(RECOVERY_VARIANT),)
 print_build_config_vars += \
   RECOVERY_VARIANT
