6518bb14d13098e76c84351e1c3b6d575c231df1
dadi11 <dadi.sikorski@gmail.com>
2017-08-26 01:07:59 +0200

    build: rebrand to XenonHD
    Change-Id: I1563b50fdfbfc08b90c0e8ed67efbe86f7078321

--- core/config.mk
+++ core/config.mk
@@ -188,7 +188,7 @@ FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 $(call project-set-path-variant,recovery,RECOVERY_VARIANT,bootable/recovery)
 
 -include vendor/extra/BoardConfigExtra.mk
--include vendor/lineage/config/BoardConfigLineage.mk
+-include vendor/xenonhd/config/BoardConfigLineage.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
@@ -966,7 +966,7 @@ include $(BUILD_SYSTEM)/ninja_config.mk
 include $(BUILD_SYSTEM)/soong_config.mk
 endif
 
-ifneq ($(LINEAGE_BUILD),)
+ifneq ($(XENONHD_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/lineage/sepolicy/common/sepolicy.mk)
@@ -978,10 +978,10 @@ $(eval include device/lineage/sepolicy/common/sepolicy.mk)
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
 
 # Rules for QCOM targets
--include $(TOPDIR)vendor/lineage/build/core/qcom_target.mk
+-include $(TOPDIR)vendor/xenonhd/build/core/qcom_target.mk
 
 # Rules for MTK targets
--include $(TOPDIR)vendor/lineage/build/core/mtk_target.mk
+-include $(TOPDIR)vendor/xenonhd/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
--- core/product_config.mk
+++ core/product_config.mk
@@ -168,9 +168,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A Lineage build needs only the Lineage product makefiles.
-ifneq ($(LINEAGE_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(LINEAGE_BUILD)/lineage.mk")
+# A XenonHD build needs only the XenonHD product makefiles.
+ifneq ($(XENONHD_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(XENONHD_BUILD)/xenonhd.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -181,9 +181,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # LINEAGE_BUILD
+endif # XENONHD_BUILD
 
-ifeq ($(LINEAGE_BUILD),)
+ifeq ($(XENONHD_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
