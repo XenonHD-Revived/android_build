e69a85f19f08a9b5add4dd64f32779e8904f9d3c
Simon Shields <simon@lineageos.org>
2018-03-20 23:02:59 +1100

    build: build generic xenonhd targets from vendor/xenonhd
    Change-Id: I89f85dfd08aa6bf03faeacedc6a91882121b0df1

--- core/product_config.mk
+++ core/product_config.mk
@@ -171,6 +171,7 @@ include $(BUILD_SYSTEM)/device.mk
 # A XenonHD build needs only the XenonHD product makefiles.
 ifneq ($(XENONHD_BUILD),)
   all_product_configs := $(shell find device -path "*/$(XENONHD_BUILD)/xenonhd.mk")
+  all_product_configs += $(wildcard vendor/xenonhd/build/target/product/xenonhd_$(XENONHD_BUILD).mk)
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
